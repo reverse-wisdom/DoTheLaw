896cd332d62ada1b1e4179ec0c7ba7710507cfff
현진혁 <wak8835@gmail.com>
2021-01-22 13:46:05 +0900

    Add S04P12D103-81 : vue-webrtc 플러그인 설치 및 적용

--- examples/vue-webrtc-test/vue-webrtc-test/src/App.vue
+++ examples/vue-webrtc-test/vue-webrtc-test/src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <vue-webrtc width="100%" roomId="roomId"> </vue-webrtc>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+  },
 };
 </script>
 
--- examples/vue-webrtc-test/vue-webrtc-test/src/main.js
+++ examples/vue-webrtc-test/vue-webrtc-test/src/main.js
@@ -1,8 +1,12 @@
 import Vue from "vue";
+import WebRTC from "vue-webrtc";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+Vue.use(WebRTC);
+Vue.component(WebRTC.name, WebRTC["vue-webrtc"]);
+
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
