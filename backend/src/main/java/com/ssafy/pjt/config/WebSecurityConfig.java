83c586f7c6d69b8afa397dcff251c7dde0681dc8
현진혁 <wak8835@gmail.com>
2021-02-02 00:58:41 +0900

    Update S04P13D103-12 : 이미지 업로드 컨트롤러 추가

--- backend/src/main/java/com/ssafy/pjt/config/WebSecurityConfig.java
+++ backend/src/main/java/com/ssafy/pjt/config/WebSecurityConfig.java
@@ -69,6 +69,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                              
                 
                 .antMatchers("/api/ocr/**").permitAll() //test
+                .antMatchers("/api/image/**").permitAll() //test
                 .anyRequest().authenticated()
 
                 .and()
