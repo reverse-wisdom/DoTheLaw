eeea3d63f59c0cb391fe616217dfb83ca215cee0
김두상 <ds021346@gmail.com>
2021-02-01 16:50:14 +0900

    Update S04P13D103-12 : 수정

--- backend/src/main/java/com/ssafy/pjt/core/service/MemberUseCase.java
+++ backend/src/main/java/com/ssafy/pjt/core/service/MemberUseCase.java
@@ -1,14 +1,18 @@
 package com.ssafy.pjt.core.service;
 
 import java.sql.SQLException;
+import java.util.List;
 
+import com.ssafy.pjt.core.security.Role;
 import com.ssafy.pjt.web.dto.MemberDTO;
 import com.ssafy.pjt.web.dto.SignupRequestDTO;
 
-public interface SignupUseCase {
+public interface MemberUseCase {
 	
 	public void joinMember(SignupRequestDTO member) throws Exception;
 	void joinLawyer(MemberDTO lawyer) throws SQLException;
 	public boolean check(String email, String name);
 	public boolean checkEmail(String email);
+	public boolean checkName(String name);
+	public boolean checkRoleAndId(Role role, int uuid, String email);
 }
