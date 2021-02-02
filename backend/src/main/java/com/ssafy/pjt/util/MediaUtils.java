b98f551f1bbf4d636b09ce4c86ea95940cd58bbe
현진혁 <wak8835@gmail.com>
2021-02-02 14:07:41 +0900

    Update S04P13D103-12 : 파일 업로드 컨트롤러 생성

--- backend/src/main/java/com/ssafy/pjt/util/MediaUtils.java
+++ backend/src/main/java/com/ssafy/pjt/util/MediaUtils.java
@@ -3,25 +3,34 @@ package com.ssafy.pjt.util;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.activation.MimetypesFileTypeMap;
+
 import org.springframework.http.MediaType;
 
 public class MediaUtils {
-    private static Map<String, MediaType> mediaMap;
-    
-    static {
-        mediaMap = new HashMap<>();
-        
-        mediaMap.put("JPEG", MediaType.IMAGE_JPEG);
-        mediaMap.put("JPG", MediaType.IMAGE_JPEG);
-        mediaMap.put("GIF", MediaType.IMAGE_GIF);
-        mediaMap.put("PNG", MediaType.IMAGE_PNG);
-    }
-    
-    public static MediaType getMediaType(String type) {
-        return mediaMap.get(type.toUpperCase());
-    }
-    
-    public static boolean containsImageMediaType(String mediaType) {
-        return mediaMap.values().contains(MediaType.valueOf(mediaType));
-    }
+	private static Map<String, MediaType> mediaMap;
+
+	static {
+		mediaMap = new HashMap<>();
+
+		mediaMap.put("jpeg", MediaType.IMAGE_JPEG);
+		mediaMap.put("jpg", MediaType.IMAGE_JPEG);
+		mediaMap.put("gif", MediaType.IMAGE_GIF);
+		mediaMap.put("png", MediaType.IMAGE_PNG);
+		mediaMap.put("bmp", new MediaType("image", "bmp"));
+		mediaMap.put("wav", new MediaType("audio", "x-wav"));
+		mediaMap.put("mp3", new MediaType("audio", "mpeg"));
+		mediaMap.put("avi", new MediaType("video", "x-msvideo"));
+		mediaMap.put("mov", new MediaType("video", "quicktime"));
+		mediaMap.put("mp4", new MediaType("video", "mp4"));
+		mediaMap.put("mpeg", new MediaType("video", "mpeg"));
+	}
+
+	public static MediaType getMediaType(String type) {
+		return mediaMap.get(type.toLowerCase());
+	}
+
+	public static boolean containsImageMediaType(String mediaType) {
+		return mediaMap.values().contains(MediaType.valueOf(mediaType));
+	}
 }
