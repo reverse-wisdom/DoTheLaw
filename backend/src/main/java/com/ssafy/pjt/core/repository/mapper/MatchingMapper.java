2d893282f2d603e5d708fc39a6cdbf1546d99bbb
김두상 <ds021346@gmail.com>
2021-02-04 16:39:57 +0900

    Update S04P13D103-84 : 불필요한 System.out 및 함수 제거, Response status 변경

--- backend/src/main/java/com/ssafy/pjt/core/repository/mapper/MatchingMapper.java
+++ backend/src/main/java/com/ssafy/pjt/core/repository/mapper/MatchingMapper.java
@@ -9,7 +9,6 @@ import com.ssafy.pjt.web.dto.MatchingRequestDTO;
 public interface MatchingMapper {
 	public List<MatchingRequestDTO> search(int lawyerUuid) throws SQLException;
 	public MatchingRequestDTO detail(int matchingId) throws SQLException;
-	//public boolean hit(int boardId) throws SQLException;
 	public boolean insert(MatchingRequestDTO matching) throws SQLException;
 	public boolean delete(int matchingId) throws SQLException;
 	public boolean update(MatchingRequestDTO matching) throws SQLException;
