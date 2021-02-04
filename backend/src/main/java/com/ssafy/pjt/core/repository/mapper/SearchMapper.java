9cde39a4aa3df2f9918f16395cecb04faa654d13
현진혁 <wak8835@gmail.com>
2021-02-04 15:19:22 +0900

    Add S04P13D103-50 : 판례 단어 조회 기능 추가

--- backend/src/main/java/com/ssafy/pjt/core/repository/mapper/SearchMapper.java
+++ backend/src/main/java/com/ssafy/pjt/core/repository/mapper/SearchMapper.java
@@ -2,11 +2,12 @@ package com.ssafy.pjt.core.repository.mapper;
 
 import java.util.List;
 
+import com.ssafy.pjt.web.dto.SearchDictDTO;
 import com.ssafy.pjt.web.dto.SearchRequestDTO;
 
 public interface SearchMapper {
 
 	void hit(String word);
 	List<SearchRequestDTO> selectHitTop();
-
+	List<SearchDictDTO> selectDictAll();
 }
