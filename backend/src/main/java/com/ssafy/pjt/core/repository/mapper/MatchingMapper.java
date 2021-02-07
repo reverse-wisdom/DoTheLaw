f10b9bb6213285322b7aa42ee93a773a97fbab18
김두상 <ds021346@gmail.com>
2021-02-07 15:57:21 +0900

    Update S04P13D103-84 : 자문 매칭 테이틀 title 추가, 수정 및 삭제 api Param  Role 삭제

--- backend/src/main/java/com/ssafy/pjt/core/repository/mapper/MatchingMapper.java
+++ backend/src/main/java/com/ssafy/pjt/core/repository/mapper/MatchingMapper.java
@@ -4,6 +4,8 @@ import java.sql.SQLException;
 import java.util.List;
 
 import com.ssafy.pjt.web.dto.MatchingRequestDTO;
+import com.ssafy.pjt.web.dto.MemberRequestDTO;
+import com.ssafy.pjt.web.dto.ProposalRequestDTO;
 
 public interface MatchingMapper {
 	public List<MatchingRequestDTO> search(int lawyerUuid) throws SQLException;
@@ -14,4 +16,6 @@ public interface MatchingMapper {
 	public boolean update(MatchingRequestDTO matching) throws SQLException;
 	public int checkUser(int matchingId) throws SQLException;
 	public int checkLawyer(int matchingId) throws SQLException;
+	
+	public MemberRequestDTO searchMember (int uuid) throws SQLException;
 }
