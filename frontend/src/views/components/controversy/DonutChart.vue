59d320d87914e269e4f5432cfa11f0a445712fd2
윤예준 <dbm01049@gmail.com>
2021-02-09 23:46:18 +0900

    Create S04P13D103-82 찬반 논쟁 기본 프론트 구현(상세 및 댓글 제외)

--- frontend/src/views/components/controversy/DonutChart.vue
+++ frontend/src/views/components/controversy/DonutChart.vue
@@ -9,6 +9,7 @@ export default {
         hoverBackgroundColor: 'red',
         hoverBorderWidth: 10,
         labels: ['찬성', '반대'],
+
         datasets: [
           {
             label: 'Data One',
@@ -16,6 +17,7 @@ export default {
             data: [],
           },
         ],
+        hoverOffset: 4,
       },
     };
   },
@@ -24,9 +26,13 @@ export default {
   },
 
   created() {
-    this.chartData.datasets[0].data.push(this.list.agree);
+    let agree = Math.round((this.list.agree / (this.list.agree + this.list.opposition)) * 100);
+    let opposition = Math.round((this.list.opposition / (this.list.agree + this.list.opposition)) * 100);
+
+    this.chartData.datasets[0].data.push(agree);
+
     this.chartData.datasets[0].backgroundColor.push('blue');
-    this.chartData.datasets[0].data.push(this.list.opposition);
+    this.chartData.datasets[0].data.push(opposition);
     this.chartData.datasets[0].backgroundColor.push('red');
   },
   mounted() {
@@ -35,10 +41,21 @@ export default {
       borderWidth: '10px',
       hoverBackgroundColor: 'red',
       hoverBorderWidth: '10px',
-      width: '50%',
-      height: '50%',
+      width: '100%',
+      height: '100%',
       rotation: 1 * Math.PI,
       circumference: 1 * Math.PI,
+      // pieceLabel: { mode: 'label', position: 'outside', fontSize: 11, fontStyle: 'bold' },
+
+      tooltips: {
+        enabled: true,
+        mode: 'index',
+        callbacks: {
+          label: function(i, data) {
+            return data.datasets[0].data[i.index] + ' %';
+          },
+        },
+      },
     });
   },
 };
