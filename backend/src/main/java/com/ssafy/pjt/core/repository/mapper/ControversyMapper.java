ab7399973bfb984e476213f52c2533666867e3b5
김두상 <ds021346@gmail.com>
2021-02-09 14:50:23 +0900

    Update S04P13D103-1 : 찬반논쟁 CRUD 수정

--- backend/src/main/java/com/ssafy/pjt/core/repository/mapper/ControversyMapper.java
+++ backend/src/main/java/com/ssafy/pjt/core/repository/mapper/ControversyMapper.java
@@ -3,13 +3,17 @@ package com.ssafy.pjt.core.repository.mapper;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+import org.springframework.dao.DataIntegrityViolationException;
+
 import com.ssafy.pjt.web.dto.ControversyDTO;
 
 public interface ControversyMapper {
 	public List<ControversyDTO> all() throws SQLException;
 	public ControversyDTO detail(int controversyId) throws SQLException;
-	public boolean hit(int controversyId) throws SQLException;
+	public boolean hit(@Param("controversyId")int controversyId, @Param("check")String check) throws SQLException;
 	public boolean insert(ControversyDTO board) throws SQLException;
 	public boolean delete(int controversyId) throws SQLException;
 	public boolean update(ControversyDTO board) throws SQLException;
+	public void overlab(@Param("uuid") int uuid, @Param("controversyId")int controversyId) throws DataIntegrityViolationException;
 }
