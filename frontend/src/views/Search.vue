5ba2e9b35210ac004a5873ac0500f13061bd7c24
윤예준 <dbm01049@gmail.com>
2021-02-13 02:59:13 +0900

    Update S04P13D103-3 css 수정

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -127,7 +127,7 @@ export default {
   props: {
     header: {
       type: String,
-      default: require('@/assets/img/jj02.gif'),
+      default: require('@/assets/img/book.jpg'),
     },
   },
   computed: {
