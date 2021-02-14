4c6043ab86c4ef841b24bea8b6f0e0aa938ee728
reverse-wisdom <pumpkintime84@gmail.com>
2021-02-15 05:23:19 +0900

    Update S04P13D103-13: 로그인 회원가입 배경이미지 디자인 및 변경

--- frontend/src/views/RegisterIndex.vue
+++ frontend/src/views/RegisterIndex.vue
@@ -39,7 +39,7 @@ export default {
   props: {
     header: {
       type: String,
-      default: require('@/assets/img/jj02.gif'),
+      default: require('@/assets/img/bg_signup.jpg'),
     },
     city: {
       type: String,
