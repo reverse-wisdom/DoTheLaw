4681261c5e4a578eab38b4ac3d37e766670769b1
현진혁 <wak8835@gmail.com>
2021-02-15 07:21:20 +0900

    Add S04P13D103-18 : 생활법령 로딩, 스크롤 기능 추가

---
+++ frontend/src/views/components/EasyLawDetailCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <hr />
+    <span class="font-weight-bold h4">
+      &lt; 요약 &gt;
+    </span>
+    <v-img :src="image"></v-img>
+    <span>{{ summary }}</span>
+    <hr />
+    <span class="font-weight-bold h4">
+      &lt; 상세 &gt;
+    </span>
+    <span v-html="content"></span>
+    <v-overlay :opacity="0" :value="overlay">
+      <v-progress-circular color="deep-orange" indeterminate size="64" :width="7">
+        Loading...
+      </v-progress-circular>
+    </v-overlay>
+  </div>
+</template>
+
+<script>
+import { getEasyLawDetail } from '@/api/easylaw.js';
+function setBookmarkCookiess(data) {
+  return false;
+}
+export default {
+  data() {
+    return { overlay: true, content: '', image: '', summary: '' };
+  },
+  props: ['no'],
+  mounted() {
+    // console.log('전달받은 no', this.no);
+    // console.dir(
+    getEasyLawDetail(this.no, (response) => {
+      this.content = response.data.content;
+      this.image = response.data.image;
+      this.summary = response.data.summary;
+      this.overlay = false;
+      this.$root.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    });
+    // );
+  },
+};
+</script>
+
+<style></style>
