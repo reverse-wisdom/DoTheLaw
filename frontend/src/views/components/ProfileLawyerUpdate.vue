4ddace30c5ae1c2f08250819a0a5382f6776c3bf
reverse-wisdom <pumpkintime84@gmail.com>
2021-02-15 05:41:53 +0900

    Update S04P13D103-13: 게시판, 찬반논쟁, 자문매칭 그외 페이지 등등 배경이미지 변경

--- frontend/src/views/components/ProfileLawyerUpdate.vue
+++ frontend/src/views/components/ProfileLawyerUpdate.vue
@@ -110,7 +110,7 @@ export default {
   props: {
     header: {
       type: String,
-      default: require('@/assets/img/jj02.gif'),
+      default: require('@/assets/img/bg_profile.jpg'),
     },
   },
   computed: {
--- frontend/src/views/components/advise/AdviseWrite.vue
+++ frontend/src/views/components/advise/AdviseWrite.vue
@@ -157,7 +157,7 @@ export default {
   props: {
     header: {
       type: String,
-      default: require('@/assets/img/jj02.gif'),
+      default: require('@/assets/img/bg_matching.png'),
     },
   },
   computed: {
