80346f673fd356874989e590c4b527afb50f0042
윤예준 <50161756+dbs7120@users.noreply.github.com>
2021-02-16 13:03:15 +0900

    Update S04P13D103-3 프론트엔드 리드미 삭제 / footer 수정

--- frontend/src/layout/MainFooter.vue
+++ frontend/src/layout/MainFooter.vue
@@ -18,10 +18,6 @@
               사이트 이용안내
             </a>
           </li>
-          <li>
-            <!-- <v-select :items="items" label="사이트맵" hide-details single-line solo style="width: 200px;"></v-select> -->
-            <v-select :items="items" menu-props="auto" hide-details label="사이트맵" single-line></v-select>
-          </li>
         </ul>
       </nav>
     </div>
